2c1c66ffcd849f3b39f9107488caa4c755cd80d7
LuisFerdinand <ferdinandluis88@gmail.com>
2024-04-07 20:38:00 +0700

    Update profile data with external image URLs
    Updated the profile data to use external image URLs for profile pictures and hobby images. This change allows for more dynamic and visually appealing profiles.

--- src/components/ProfileCard.vue
+++ src/components/ProfileCard.vue
@@ -2,24 +2,38 @@
 import { ref } from 'vue'
 
 const profileData = ref([
-      {
-        picture: "home.png",
-        fullname: "Joko Keladi",
-        address: "Jl. Meteor VI No. 198",
-        hobi: ["futsal.jpg", "catur.jpg"]
-      },
-      {
-        picture: "water.png",
-        fullname: "Alex Silalahi",
-        address: "Jl. Pandan X No. 30",
-        hobi: ["renang.jpg", "tinju.jpg", "sepak bola.jpg"]
-      },
-      {
-        picture: "star.png",
-        fullname: "Subir Kompani",
-        address: "Jl. Sekar Buana V No. 11",
-        hobi: ["bulu_tangkis.jpg", "futsal.jpg", "catur.jpg"]
-      }
+  {
+    picture:
+      'https://media.licdn.com/dms/image/D5603AQHx_hCVsyya_Q/profile-displayphoto-shrink_800_800/0/1694454119626?e=2147483647&v=beta&t=U5EkoQIrnfHTZDB0jdPC9IZTdQ6Qh6FeInKdMO7Ddyk',
+    fullname: 'Joko Keladi',
+    address: 'Jl. Meteor VI No. 198',
+    hobi: [
+      'https://upload.wikimedia.org/wikipedia/commons/0/06/Futebol_Salao_Pan2007.jpg',
+      'https://play-lh.googleusercontent.com/vj2IpdfGBoz5uIEY0UjGqDhSghy9IpnHuJm7zztuxZaNhLhgmTwX6QVX9f4mxCpm-GI=w526-h296-rw'
+    ]
+  },
+  {
+    picture:
+      'https://media.licdn.com/dms/image/C5603AQH41Hm2YNP3IQ/profile-displayphoto-shrink_400_400/0/1647149823202?e=2147483647&v=beta&t=-vx3Jq0pV8roUER0Hjjl-MYHowfESRz-GKlJWEpscQE',
+    fullname: 'Alex Silalahi',
+    address: 'Jl. Pandan X No. 30',
+    hobi: [
+      'https://www.health.com/thmb/Yv4HuoQyNbHNNxgtOTm63zqxurQ=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Health-Swimming-080c78802f384a4687df5a3b13d5611e-3719a8e40a3c4c43a63a4d795e47c505.jpg',
+      'https://a.espncdn.com/media/motion/2023/1028/dm_231028_dm_231028_BOX_Fury_Ngannou_pub2tag/dm_231028_dm_231028_BOX_Fury_Ngannou_pub2tag.jpg',
+      'https://i.ytimg.com/vi/BSZy2MorHTk/maxresdefault.jpg'
+    ]
+  },
+  {
+    picture:
+      'https://image.cnbcfm.com/api/v1/image/103979967-20160928-0666-2283.jpg?v=1530879569&ffmt=webp&w=400&h=300',
+    fullname: 'Subir Kompani',
+    address: 'Jl. Sekar Buana V No. 11',
+    hobi: [
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQsaOcgNjhLCOacn8yXk1dCNbM7LoA10hrV4f8zTf7m1Q&s',
+      'https://upload.wikimedia.org/wikipedia/commons/0/06/Futebol_Salao_Pan2007.jpg',
+      'https://play-lh.googleusercontent.com/vj2IpdfGBoz5uIEY0UjGqDhSghy9IpnHuJm7zztuxZaNhLhgmTwX6QVX9f4mxCpm-GI=w526-h296-rw'
+    ]
+  }
 ])
 </script>
 
