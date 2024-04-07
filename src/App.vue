a6c7c3537bfc8f835d5b97e6e033c77fa6a76f94
LuisFerdinand <ferdinandluis88@gmail.com>
2024-04-07 20:11:13 +0700

    Creating Component
    Use the container that later will be matches to the assignment using CSS, using the data given.

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup>
+import ProfileCard from './components/ProfileCard.vue';
+</script>
+
+<template>
+  <ProfileCard />
+</template>
---
+++ src/components/ProfileCard.vue
@@ -0,0 +1,50 @@
+<script setup>
+import { ref } from 'vue'
+
+const profileData = ref([
+      {
+        picture: "home.png",
+        fullname: "Joko Keladi",
+        address: "Jl. Meteor VI No. 198",
+        hobi: ["futsal.jpg", "catur.jpg"]
+      },
+      {
+        picture: "water.png",
+        fullname: "Alex Silalahi",
+        address: "Jl. Pandan X No. 30",
+        hobi: ["renang.jpg", "tinju.jpg", "sepak bola.jpg"]
+      },
+      {
+        picture: "star.png",
+        fullname: "Subir Kompani",
+        address: "Jl. Sekar Buana V No. 11",
+        hobi: ["bulu_tangkis.jpg", "futsal.jpg", "catur.jpg"]
+      }
+])
+</script>
+
+<template>
+  <div class="card-container">
+    <div v-for="(profile, index) in profileData" :key="index" class="card">
+      <div class="profile-info">
+        <div class="picture">
+          <img :src="profile.picture" alt="Profile Picture" />
+        </div>
+        <div class="fullname-address">
+          <p >Fullname: {{ profile.fullname }}</p>
+          <p >Address: {{ profile.address }}</p>
+        </div>
+      </div>
+      <div class="hobbies">
+        <p class="profile-hobbies">Hobbies:</p>
+        <div class="hobbies-list">
+          <img v-for="hobby in profile.hobi" :src="hobby" :key="hobby" alt="Hobby Image" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style> 
