3117575d95607b4a2f663596a789d0b2454b15fb
LuisFerdinand <ferdinandluis88@gmail.com>
2024-04-07 20:36:06 +0700

    Style: Improve layout and styling of profile cards
    efactored CSS styles to enhance the layout and appearance of profile cards. Changes include:
    - Added 'align-items: center;' to '.card-container' for better alignment
    - Changed '.card' to 'display: flex; flex-direction: column;' for a vertical layout
    - Added padding and fixed width to '.card' for consistent card sizing
    - Adjusted styles for '.profile-info' and '.picture' to improve spacing and alignment
    - Updated styles for '.fullname-address' and '.hobbies' for better readability and visual appeal
    - Added styles for '.profile-hobbies', '.hobbies-list', and '.hobbies-list img' for consistent display of hobbies
    - Modified '.profile-info > *' for consistent border styling
    
    These changes address layout and styling issues, improving the overall design consistency and user experience.

--- src/components/ProfileCard.vue
+++ src/components/ProfileCard.vue
@@ -49,32 +49,66 @@ const profileData = ref([
 .card-container {
   display: flex;
   flex-direction: column;
+  align-items: center;
 }
 
 .card {
   margin-top: 5px;
   display: flex;
+  flex-direction: column;
   border: 1px solid hsl(0, 0%, 80%);
-  width: auto;
+  padding: 10px;
+  width: 400px;
 }
 
 .profile-info {
   display: flex;
-  margin-right: auto;
+  align-items: center;
 }
 
 .picture {
   flex: 1;
-  margin-right: auto;
+  margin-right: 10px;
 }
 
+.picture img {
+  width: 150px;
+  height: 150px;
+  object-fit: cover;
+}
 
 .fullname-address {
   flex: 2;
+  border-left: 1px solid hsl(0, 0%, 80%);
+  padding-left: 10px;
 }
 
 .hobbies {
   margin-top: 10px;
+  border-top: 1px solid hsl(0, 0%, 80%);
   padding-top: 10px;
+}
+
+.profile-hobbies {
+  font-weight: bold;
+  margin-bottom: 5px;
+}
+
+.hobbies-list {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.hobbies-list img {
+  width: 70px;
+  height: 70px;
+  object-fit: cover;
+  margin-right: 5px;
+  margin-bottom: 5px;
+}
+
+.profile-info > * {
+  border-right: 1px solid hsl(0, 0%, 80%);
+  padding-right: 10px;
 }
 </style> 
